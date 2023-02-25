a9f73eb524ca43d651e61cd02d1213ea1694bb1a
Ilya <86732040+GitIcon65@users.noreply.github.com>
2023-02-25 22:24:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,73 @@
-import random
-import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-import nltk
+
 from keras.models import load_model
-from nltk.stem import WordNetLemmatizer
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json', encoding = "utf-8").read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
 
 import telebot
     
-
-API_TOKEN = "5898955683:AAG-VEooW2vIoKV3Diz36Ci1VomR-U-axLQ"
+nltk.download("punkt")
+nltk.download("wordnet")
 
 bot = telebot.TeleBot(token = API_TOKEN)
 
-nltk.download("punkt")
-
-lemmatizer = WordNetLemmatizer()
-
-intents = json.loads(open("intents_ru.json", encoding="utf-8").read())
-words = pickle.load(open("model/words.pkl", "rb"))
-classes = pickle.load(open("model/classes.pkl", "rb"))
-model = load_model("model/savily_bot.h5")
-
-def clean_up_sentences(sentence):
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-def bagw(sentence):
-    sentence_words = clean_up_sentences(sentence)
-    bag = [0] * len(words)
-    for w in sentence_words:
-        for i, word in enumerate(words):
-            if word == w:
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
-    
-    return np.array(bag)
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
 
 def predict_class(sentence):
-    bow = bagw(sentence)
-    res = model.predict(np.array([bow]))[0]
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    result = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    result.sort(key=lambda x: x[1], reverse=True)
-    result_list = []
-    for r in result:
-        result_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
 
-        return result_list
-
-def get_response(intent_lists, intents_json):
-    try:
-        tag = intent_lists[0]["intent"]
-        list_of_intents = intents_json["intents"]
-        result = ""
-        for i in list_of_intents:
-            if i["tag"] == tag:
-                result = random.choice(i["responses"])
-                break
-    except:
-        result = "Извените я вас плохо поняла"
-    
-    #result = uni2txt(result)
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
     return result
+
+def chatbot_response(msg, intents):
+    ints = predict_class(msg)
+    res = getResponse(ints, intents)
+    return res
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
@@ -70,9 +75,11 @@ def send_welcome(message):
 
 @bot.message_handler(func = lambda message: True)
 def on_message(message):
+    f = open("logs/" + str(message.chat.first_name) + "_" + str(message.chat.last_name) + "_" + str(message.chat.id) + "_log.txt", "a", encoding = "UTF-8")
     msg = message.text
-    ints = predict_class(msg)
-    res = get_response(ints, intents)
-    bot.send_message(message.chat.id, res)
+    res = chatbot_response(msg, intents)
+    f.write("u: " + message.text + "\n" + res + "\n")
+    f.close()
+    bot.reply_to(message, res)
 
 bot.infinity_polling()
